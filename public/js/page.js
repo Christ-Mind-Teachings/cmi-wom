c9d931ead671252d0d1aef822b9926235f2882f7
Rick Mercer <rmercer33@gmail.com>
2019-03-15 11:53:41 +0800

    $message

--- public/js/page.js
+++ public/js/page.js
@@ -37231,7 +37231,7 @@ function showSavedQuery() {
 
 const page = __webpack_require__(20);
 
-const queryResultName = "query-result-wom";
+const queryResultName = "query-result-raj";
 const SCROLL_INTERVAL = 250;
 
 function scrollComplete(message, type) {
@@ -37386,11 +37386,11 @@ function initClickListeners(matches) {
 */
 function findPositions(pid, pageKey, flat) {
   let positions = {
-    current: -1,
-    prev: -1,
-    start: -1,
-    end: -1,
-    next: -1
+    current: -1, //current para with search match
+    prev: -1, //previous page with search match
+    start: -1, //first para with match on page
+    end: -1, //last para with match on page
+    next: -1 //next page with search match
   };
 
   let found = false;
@@ -37401,16 +37401,21 @@ function findPositions(pid, pageKey, flat) {
         positions.current = i;
       }
       if (!found) {
+        //first match on page
         positions.start = i;
         positions.end = i;
         found = true;
 
         if (i > 0) {
+          //the previous page with a match
           positions.prev = i - 1;
         }
+      } else {
+        //more than one match on the page
+        positions.end = i;
       }
     } else if (found) {
-      positions.end = i - 1;
+      //positions.end = i - 1;
       positions.next = i;
       break;
     }
