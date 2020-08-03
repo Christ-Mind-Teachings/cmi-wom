cfbba5a221e2a7dd30b2d7c4ca00a40724c10c4f
Rick Mercer <rmercer33@gmail.com>
2020-08-03 09:45:28 -1000

    tweeks to support account user features

--- src/js/modules/_about/about.js
+++ src/js/modules/_about/about.js
@@ -37,7 +37,9 @@ function createClickHandlers() {
     if ($(this).hasClass("contact-me")) {
       location.href = "/acq/contact/";
     }
+  });
 
+  $(".login-menu-option-account").on("click", "div.item", function(e) {
     if ($(this).hasClass("profile-management")) {
       location.href = "/profile/email/";
     }
@@ -45,7 +47,6 @@ function createClickHandlers() {
     if ($(this).hasClass("topic-management")) {
       location.href = "/profile/topicMgt/";
     }
-
   });
 
   //quick links
